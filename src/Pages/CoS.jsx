78b698f69f98114e47c64fc4d16efe06e9e1be9a
phylosick <garvitkhare2004@gmail.com>
2024-01-07 00:32:00 +0530

    COS card modified

--- src/Pages/CoS.jsx
+++ src/Pages/CoS.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Card from '../components/COS/Card';
 
+
 const Chamber = () => {
     return (
         <h1>
