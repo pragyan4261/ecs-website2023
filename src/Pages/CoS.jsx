69320e3635d96e4e5b0fde52fdbfc9fbbea6650e
phylosick <garvitkhare2004@gmail.com>
2024-01-06 23:47:23 +0530

    some changes made

--- src/Pages/CoS.jsx
+++ src/Pages/CoS.jsx
@@ -1,7 +1,14 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import Card from '../components/COS/Card';
+
 const Chamber = () => {
     return (
         <h1>
             Chamber of secrets
+            <Navbar home='inactive' events='active' developers='inactive' about='inactive' feed='inactive' members='inactive' />
+            <Card question={"Question"} answer={"answer"} />
         </h1>
     );
 }
