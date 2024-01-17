035e6a740f6810edc7a07956b3744b4d3cdfa32b
Pragyan Das <91261642+pragyan4261@users.noreply.github.com>
2024-01-17 16:04:04 +0530

    Update Card.jsx

--- src/cards/Spectrum/Card.jsx
+++ src/cards/Spectrum/Card.jsx
@@ -2,10 +2,10 @@ import "./Card.css"
 const Card = (props) => {
     return (
         <>
-            <a href={props.register} target="_blank" className='card flex flex-col justify-center items-center text-[#ffffff] w-[18rem] h-[18rem] md:w-[25rem] md:h-[20rem] rounded-[1rem]' style={{ backgroundImage: `url('${props.photo}')`,backgroundColor:'#00000055',backgroundBlendMode:'darken', backgroundSize: 'cover', backgroundPosition: 'center', margin: '2rem auto' }}>
+            <div className='card flex flex-col justify-center items-center text-[#ffffff] w-[18rem] h-[18rem] md:w-[25rem] md:h-[20rem] rounded-[1rem]' style={{ backgroundImage: `url('${props.photo}')`,backgroundColor:'#00000055',backgroundBlendMode:'darken', backgroundSize: 'cover', backgroundPosition: 'center', margin: '2rem auto' }}>
                 <h1 className="cardName text-[2rem]">{props.name}</h1>
-                <p className="content text-[1.2rem]" style={{border:'1px solid #ffffff',borderRadius:'2rem',padding:'1rem'}}>REGISTER HERE</p>
-            </a>
+                <a href={props.register} target="_blank"  className="content text-[1.2rem]" style={{border:'1px solid #ffffff',borderRadius:'2rem',padding:'1rem'}}>REGISTER HERE</p>
+            </div>
         </>
     );
 }
